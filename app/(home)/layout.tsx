d584e4bcd4082add74b4ef2b016bdde592a50446
Zack <muhammadbinasim471@gmail.com>
2025-02-10 12:47:17 +0000

    fix: fixed Website Auto Using Light Mode
    The website used to auto use light mode. The theme switch button didnt work for the first 2 presses
    aswell

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <Head>
         <title>Chronium | Home</title>
         <meta name="description" content="All-In-One discord bot which is 100% free!" />
